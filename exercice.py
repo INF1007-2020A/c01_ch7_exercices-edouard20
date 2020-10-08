101c0b3e7064ed275c2ec070929e329e4755e6fd
EdouardFrechette <71038294+edouard20@users.noreply.github.com>
2020-10-08 08:18:22 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,13 +2,21 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
+import math
+import turtle
 
+def calcul_ellipsoide(a:float, b:float, c:float, masse_volumique: float) -> tuple:
+    volume = 4/3 * math.pi * a * b * c
+    return volume, volume * masse_volumique
+
+# def arbre_recursivité():
+#     turtle.
+# fonction qui cherche si la chaine nest pas null et si elle contient seulement des a t c g
+def chaine_valide(chaine: str):
 
 
 # TODO: Définissez vos fonction ici
 
-
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-    pass
+    print(calcul_ellipsoide(4,5,6,7))
